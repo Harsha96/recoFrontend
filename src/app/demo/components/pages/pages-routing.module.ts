ad58f6b2d67f10b569cd601dc8db51d7ab5934bf
Kaif Muhammed <kaif@entgra.io>
2023-06-13 18:56:59 +0530

    added question UI

--- src/app/demo/components/pages/pages-routing.module.ts
+++ src/app/demo/components/pages/pages-routing.module.ts
@@ -1,15 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
-    imports: [RouterModule.forChild([
-        { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
-        { path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule) },
-        { path: 'timeline', loadChildren: () => import('./timeline/timelinedemo.module').then(m => m.TimelineDemoModule) },
-        { path: 'degree', loadChildren: () => import('./degreeRecomendation/degree.module').then(m => m.DegreeModule) },
+    imports: [
+        RouterModule.forChild([
+            {
+                path: 'crud',
+                loadChildren: () =>
+                    import('./crud/crud.module').then((m) => m.CrudModule),
+            },
+            {
+                path: 'empty',
+                loadChildren: () =>
+                    import('./empty/emptydemo.module').then(
+                        (m) => m.EmptyDemoModule
+                    ),
+            },
+            {
+                path: 'timeline',
+                loadChildren: () =>
+                    import('./timeline/timelinedemo.module').then(
+                        (m) => m.TimelineDemoModule
+                    ),
+            },
+            {
+                path: 'degree',
+                loadChildren: () =>
+                    import('./degreeRecomendation/degree.module').then(
+                        (m) => m.DegreeModule
+                    ),
+            },
+            { path: 'question', component: QuestionsComponent },
 
-        { path: '**', redirectTo: '/notfound' }
-    ])],
-    exports: [RouterModule]
+            { path: '**', redirectTo: '/notfound' },
+        ]),
+    ],
+    exports: [RouterModule],
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {}
